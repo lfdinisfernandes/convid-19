dd2bd3a743aa8cf28048b9acfe97eb15ca17d763
lfdinisfernandes <lfdinisfernandes@gmail.com>
2021-01-30 08:49:07 -0300

    buscando dados

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
 const port = 3000
 
-app.get('/teste', (req, res) => { 
-  res.send('Hello World!')
+app.get('/', async (req, res) => { 
+   const response = await axios.get( "https://brasil.io/covid19/cities/cases/" )
+   res.send(JSON.stringify(response))
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
