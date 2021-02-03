3b763c7a1f9bf3f9a1fdabd40a4f948d55caf580
lfdinisfernandes <lfdinisfernandes@gmail.com>
2021-02-03 10:24:54 -0300

    JavaScript For Loop

--- index.js
+++ index.js
@@ -20,16 +20,38 @@ app.get('/covid/cidades', async (req, res) => {
   db.query( sql, ( err, rows ) => {
     console.log(rows);
     res.send(JSON.stringify(rows));
-
+  
   } );
- //console.log(response)
-// res.send(JSON.stringify(response));
-});
+})
+
+
 
 app.get('/preenche-banco', async (req, res) => {
 //   const conn = await connection(db).catch(e => {}) 
    const response = await axios.get( "https://brasil.io/covid19/cities/cases/" )
-     var sql = `INSERT INTO cadastros 
+ 
+   let values= ""
+   Object.entries(response.data.cities).forEach(([key, val]) => {
+     values += `( 
+     "${response.data.cities[key].city}",
+     "${response.data.cities[key].city_ibge_code}",
+     "${response.data.cities[key].city_str}",
+     "${response.data.cities[key].confirmed}",
+     "${response.data.cities[key].confirmed_per_100k_inhabitants}",
+     "${response.data.cities[key].date}",
+     "${response.data.cities[key].date_str}",
+     "${response.data.cities[key].deaths}",
+     "${response.data.cities[key].deaths_per_100k_inhabitants}",
+     "${response.data.cities[key].death_rate_percent}",
+     "${response.data.cities[key].estimated_population}",
+     "${response.data.cities[key].state}",
+     ),`
+   })
+
+   
+
+   var sql = `INSERT INTO cadastros 
+    
      (city,
       city_ibge_code,
       city_str,
@@ -41,29 +63,22 @@ app.get('/preenche-banco', async (req, res) => {
       deaths_per_100k_inhabitants,
       death_rate_percent,
       estimated_population,
-      state) VALUES
-     ("${response.data.cities['1100015'].city}",
-      "${response.data.cities['1100015'].city_ibge_code}",
-      "${response.data.cities['1100015'].city_str}",
-      "${response.data.cities['1100015'].confirmed}",
-      "${response.data.cities['1100015'].confirmed_per_100k_inhabitants}",
-      "${response.data.cities['1100015'].date}",
-      "${response.data.cities['1100015'].date_str}",
-      "${response.data.cities['1100015'].deaths}",
-      "${response.data.cities['1100015'].deaths_per_100k_inhabitants}",
-      "${response.data.cities['1100015'].death_rate_percent}",
-      "${response.data.cities['1100015'].estimated_population}",
-      "${response.data.cities['1100015'].state}"
-      )`;
-    await db.query(sql);
-   // res.json(response);
+      state) VALUE ${values.slice(0, -1)}`
+      
 
-   // const rows = await db.query( 'SELECT * FROM cadastro' );
+
+      
+      db.query( sql, ( err, rows ) => {
+        res.send("dados inseridos")
+      
+      } );
    
-  res.send("dados inseridos")
+   
+  
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
 
