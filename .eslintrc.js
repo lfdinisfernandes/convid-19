c4017b44841dc439b03f29fb2ffa798177bedbf4
lfdinisfernandes <lfdinisfernandes@gmail.com>
2021-02-06 10:06:43 -0300

    aplicando

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,12 @@
 module.exports = {
   env: {
-    es6: true,
-    node: true,
+    browser: true,
+    commonjs: true,
+    es2021: true,
   },
-  extends: ['prettier'],
-  plugins: ['prettier'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
+  extends: 'eslint:recommended',
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
+    ecmaVersion: 12,
   },
-  rules: {
-    'prettier/prettier': 'error',
-    'class-methods-use-this': 'off',
-    'no-param-reassign': 'off',
-    camelcase: 'off',
-    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
-  },
+  rules: {},
 };
--- index.js
+++ index.js
@@ -1,42 +1,38 @@
-const express = require('express')
-const axios = require('axios')
-const app = express()
-const mysql = require('mysql')
-const port = 3000
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const mysql = require('mysql');
+const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-  var db = mysql.createConnection({
+var db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'covid-19'
+  database: 'covid-19',
+});
 
-  
-})
+db.connect();
 
-db.connect()
-
-  app.get('/buscar/dados', async (req, res) => {
-  var sql = `SELECT * FROM cadastros`; 
-  await db.query( sql, ( err, rows ) => {
-    console.log(rows);
+app.get('/buscar/dados', async (req, res) => {
+  var sql = `SELECT * FROM cadastros`;
+  await db.query(sql, (err, rows) => {
     res.send(JSON.stringify(rows));
-  
   });
-})
+});
 app.get('/covid/cidade', async (req, res) => {
   var sql = `SELECT * FROM cadastros WHERE city LIKE "%${req.query.nome}%" OR id="${req.query.id}"`;
-  await db.query( sql, ( err, rows ) => {
-   res.send(JSON.stringify(rows));
+  await db.query(sql, (err, rows) => {
+    res.send(JSON.stringify(rows));
   });
-})
+});
 
-  app.get('/preenche-dados', async (req, res) => {
-    const response = await axios.get( "https://brasil.io/covid19/cities/cases/" )     
-   let values= ""
-   Object.entries(response.data.cities).forEach(([key, val]) => {
-     values += `( 
+app.get('/preenche-dados', async (req, res) => {
+  const response = await axios.get('https://brasil.io/covid19/cities/cases/');
+  let values = '';
+  Object.entries(response.data.cities).forEach(([key]) => {
+    values += `( 
      "${response.data.cities[key].city}",
      "${response.data.cities[key].city_ibge_code}",
      "${response.data.cities[key].city_str}",
@@ -49,9 +45,9 @@ app.get('/covid/cidade', async (req, res) => {
      "${response.data.cities[key].death_rate_percent}",
      "${response.data.cities[key].estimated_population}",
      "${response.data.cities[key].state}"
-     ),`
-   })
-   var sql = `INSERT INTO cadastros      
+     ),`;
+  });
+  var sql = `INSERT INTO cadastros      
      (city,
       city_ibge_code,
       city_str,
@@ -64,31 +60,26 @@ app.get('/covid/cidade', async (req, res) => {
       death_rate_percent,
       estimated_population,
       state )
-      VALUE ${values.slice(0, -1)}`
-   
-      db.query( sql, ( err, rows ) => {
-        
-        res.send("dados inseridos")
-      
-      });
-})
+      VALUE ${values.slice(0, -1)}`;
 
-  app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  db.query(sql, () => {
+    res.send('dados inseridos');
+  });
+});
 
-  app.delete('/covid/cidade/:id', function(req, resp)  {
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.delete('/covid/cidade/:id', function (req, resp) {
   var sql = `DELETE FROM cadastros WHERE id = "${req.params.id}"`;
-  db.query( sql, ( err, rows ) => {
-    console.log(rows);
-    resp.send("dados deletado");
+  db.query(sql, () => {
+    resp.send('dados deletado');
+  });
+});
 
-} );
-})
-
-  app.post('/covid/cidade/', async (req, resp) => {
-    const values = 
-    `( 
+app.post('/covid/cidade/', async (req, resp) => {
+  const values = `( 
       "${req.body.city}",
       "${req.body.city_ibge_code}",
       "${req.body.city_str}",
@@ -101,7 +92,7 @@ app.get('/covid/cidade', async (req, res) => {
       "${req.body.death_rate_percent}",
       "${req.body.estimated_population}",
       "${req.body.state}"
-      )`
+      )`;
   var sql = `INSERT INTO cadastros      
    (city,
     city_ibge_code,
@@ -115,12 +106,11 @@ app.get('/covid/cidade', async (req, res) => {
     death_rate_percent,
     estimated_population,
     state )
-    VALUE ${values}`
-    
-      await db.query( sql, ( err, rows ) => {
-        console.log(err)
-        resp.send("Dados adicionados");
-    } );
+    VALUE ${values}`;
+
+  await db.query(sql, () => {
+    resp.send('Dados adicionados');
+  });
 });
 
 app.put('/covid/cidade/:id', async (req, resp) => {
@@ -138,10 +128,8 @@ app.put('/covid/cidade/:id', async (req, resp) => {
   deaths_per_100k_inhabitants = "${req.body.deaths_per_100k_inhabitants}",
   death_rate_percent = "${req.body.death_rate_percent}",
   estimated_population = "${req.body.estimated_population}"
-  WHERE id = "${req.params.id}"`
-  console.log(sql)
-    await db.query( sql, ( err, rows ) => {
-      //console.log(err)
-      resp.send("Dados alterados");
-  } );
+  WHERE id = "${req.params.id}"`;
+  await db.query(sql, () => {
+    resp.send('Dados alterados');
+  });
 });
